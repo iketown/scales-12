b805e2be0462053add9de551e841ee44bbd85503
ike76 <ike76@me.com>
2018-09-06 07:11:49 -0500

    default to intropage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import IntroPage from "./pages/IntroPage.jsx";
 import Page1 from "./pages/Page1.jsx";
 import Page2 from "./pages/Page2.jsx";
 import Page3 from "./pages/Page3.jsx";
@@ -10,9 +9,6 @@ import Page4 from "./pages/Page4.jsx";
 import Page5 from "./pages/Page5.jsx";
 import Page6 from "./pages/Page6";
 import Page7 from "./pages/Page7";
-import AnimatedGrid from "./components/AnimatedGrid";
-import IntroPage from "./pages/IntroPage.jsx";
-import SimpleSlider from "./pages/slickTest.jsx";
 import ShapesQuiz1 from "./pages/ShapesQuiz1";
 import ShapesQuiz2 from "./pages/ShapesQuiz2";
 
@@ -21,6 +17,7 @@ class App extends Component {
     return (
       <Router>
         <Switch>
+          <Route path="/" exact component={IntroPage} />
           <Route path="/intro" exact component={IntroPage} />
           <Route path="/page1" component={Page1} />
           <Route path="/page2" component={Page2} />
@@ -31,8 +28,6 @@ class App extends Component {
           <Route path="/page7" component={Page7} />
           <Route path="/shapesQuiz1" component={ShapesQuiz1} />
           <Route path="/shapesQuiz2" component={ShapesQuiz2} />
-          <Route path="/gridTest" component={AnimatedGrid} />
-          <Route path="/slicktest" component={SimpleSlider} />
         </Switch>
       </Router>
     );
